123386fa68616bc5ce7e3fe13e133ee9ea303cb1
bytes7bytes7 <bytes7bytes7@gmail.com>
2021-07-21 01:48:39 +0300

    added new (not good) layout

--- lib/widgets/number_panel.dart
+++ lib/widgets/number_panel.dart
@@ -7,129 +7,150 @@ class NumberPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
+    final Size size = MediaQuery.of(context).size;
+    final double height = size.height * 0.475;
+    const double buttonWidth = 70.0;
+    return LayoutBuilder(builder: (context, constraints) {
+      final double xSpace = (constraints.maxWidth - 3 * buttonWidth) / 2;
+      final double ySpace = (height - 4 * buttonWidth) / 3;
+      final double space = (xSpace < ySpace) ? xSpace : ySpace;
+      return Column(
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: '789'.characters.map((e) {
-              return Container(
-                height: 70.0,
-                width: 70.0,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).splashColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Theme.of(context).focusColor.withOpacity(0.25),
-                      offset: const Offset(0, 0),
-                      blurRadius: 10,
-                      spreadRadius: 0,
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: Text(
-                    e,
-                    style: Theme.of(context).textTheme.bodyText1,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: '7 8 9'.characters.map((e) {
+              if (e == ' ') {
+                return SizedBox(width: space);
+              } else {
+                return Container(
+                  height: buttonWidth,
+                  width: buttonWidth,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).splashColor,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).focusColor.withOpacity(0.25),
+                        offset: const Offset(0, 0),
+                        blurRadius: 10,
+                        spreadRadius: 0,
+                      ),
+                    ],
                   ),
-                ),
-              );
+                  child: Center(
+                    child: Text(
+                      e,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                );
+              }
             }).toList(),
           ),
-          const SizedBox(height: 30.0),
+          SizedBox(height: space),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: '456'.characters.map((e) {
-              return Container(
-                height: 70.0,
-                width: 70.0,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).splashColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Theme.of(context).focusColor.withOpacity(0.25),
-                      offset: const Offset(0, 0),
-                      blurRadius: 10,
-                      spreadRadius: 0,
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: Text(
-                    e,
-                    style: Theme.of(context).textTheme.bodyText1,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: '7 8 9'.characters.map((e) {
+              if (e == ' ') {
+                return SizedBox(width: space);
+              } else {
+                return Container(
+                  height: buttonWidth,
+                  width: buttonWidth,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).splashColor,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).focusColor.withOpacity(0.25),
+                        offset: const Offset(0, 0),
+                        blurRadius: 10,
+                        spreadRadius: 0,
+                      ),
+                    ],
                   ),
-                ),
-              );
+                  child: Center(
+                    child: Text(
+                      e,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                );
+              }
             }).toList(),
           ),
-          const SizedBox(height: 30.0),
+          SizedBox(height: space),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: '123'.characters.map((e) {
-              return Container(
-                height: 70.0,
-                width: 70.0,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).splashColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Theme.of(context).focusColor.withOpacity(0.25),
-                      offset: const Offset(0, 0),
-                      blurRadius: 10,
-                      spreadRadius: 0,
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: Text(
-                    e,
-                    style: Theme.of(context).textTheme.bodyText1,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: '7 8 9'.characters.map((e) {
+              if (e == ' ') {
+                return SizedBox(width: space);
+              } else {
+                return Container(
+                  height: buttonWidth,
+                  width: buttonWidth,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).splashColor,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).focusColor.withOpacity(0.25),
+                        offset: const Offset(0, 0),
+                        blurRadius: 10,
+                        spreadRadius: 0,
+                      ),
+                    ],
                   ),
-                ),
-              );
+                  child: Center(
+                    child: Text(
+                      e,
+                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                  ),
+                );
+              }
             }).toList(),
           ),
-          const SizedBox(height: 30.0),
+          SizedBox(height: space),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [',', '0', Icons.backspace_outlined].map((e) {
-              return Container(
-                height: 70.0,
-                width: 70.0,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).splashColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Theme.of(context).focusColor.withOpacity(0.25),
-                      offset: const Offset(0, 0),
-                      blurRadius: 10,
-                      spreadRadius: 0,
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: (e is IconData)
-                      ? Icon(
-                          e,
-                          size: 28.0,
-                          color: Theme.of(context).focusColor,
-                        )
-                      : Text(
-                          e.toString(),
-                          style: Theme.of(context).textTheme.bodyText1,
-                        ),
-                ),
-              );
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [',', ' ', '0', ' ', Icons.backspace_outlined].map((e) {
+              if (e == ' ') {
+                return SizedBox(width: space);
+              } else {
+                return Container(
+                  height: buttonWidth,
+                  width: buttonWidth,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).splashColor,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Theme.of(context).focusColor.withOpacity(0.25),
+                        offset: const Offset(0, 0),
+                        blurRadius: 10,
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: (e is IconData)
+                        ? Icon(
+                            e,
+                            size: 28.0,
+                            color: Theme.of(context).focusColor,
+                          )
+                        : Text(
+                            e.toString(),
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                  ),
+                );
+              }
             }).toList(),
           ),
         ],
-      ),
-    );
+      );
+    });
   }
 }
